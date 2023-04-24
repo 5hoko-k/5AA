43fa15ebd0fe92511910408926a04321c7fb43d8
Shoko Kimera <shokokimera@gmail.com>
2023-04-24 18:22:20 +0300

    add query

--- src/DisplayProgress.jsx
+++ src/DisplayProgress.jsx
@@ -1,18 +1,39 @@
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql } from "@apollo/client";
 
-var query = gql``;
+var query = gql`
+  query {
+    MediaListCollection(
+      userId: 853967
+      type: ANIME
+      status: CURRENT
+      sort: UPDATED_TIME_DESC
+    ) {
+      lists {
+        entries {
+          progress
+          updatedAt
+          media {
+            title {
+              english
+            }
+            episodes
+            coverImage {
+              large
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 function DisplayProgress() {
-    const { loading, error, data } = useQuery(query);
+  const { loading, error, data } = useQuery(query);
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error : {error.message}</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
 
-    return (
-      <>
-
-      </>
-     );
+  return <></>;
 }
 
 export default DisplayProgress;
