72d04fefc3fdaf33144a0111c4ba9e304f8cb0e7
Shoko Kimera <shokokimera@gmail.com>
2023-04-25 19:36:40 +0300

    add BrowserRouter to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './Home'
-import NavBar from './NavBar'
-import './styles/index.css'
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home";
+import NavBar from "./NavBar";
+import "./styles/index.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 
 const client = new ApolloClient({
-  uri: 'https://graphql.anilist.co',
+  uri: "https://graphql.anilist.co",
   cache: new InMemoryCache(),
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <div>
-        <NavBar />
-        <Home />
-      </div>
+      <NavBar />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>
-)
+);
