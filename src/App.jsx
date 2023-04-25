a3188769962dc1c2de4cef28c5dcc8da1f047908
Shoko Kimera <shokokimera@gmail.com>
2023-04-25 19:55:04 +0300

    add paths and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import About from "./pages/About";
 import Home from "./pages/Home";
+import Library from "./pages/Library";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='' element= { <Library /> } />
-        <Route path='' element= { <About /> } />
+        <Route path='/library' element= { <Library /> } />
+        <Route path='/about' element= { <About /> } />
       </Routes>
     </>
   );
