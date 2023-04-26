914fa1b243f1eee65ef20632fbfa724e665e969a
Shoko Kimera <shokokimera@gmail.com>
2023-04-26 16:09:21 +0300

    add filter component

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -3,7 +3,30 @@ import DisplayLibrary from "../components/DisplayLibrary";
 export default function Library() {
   return (
     <>
+      <Filter />
       <DisplayLibrary />
     </>
   );
 }
+
+const Filter = () => {
+  return (
+    <>
+      <form>
+        <label htmlFor="category">Status:</label>
+        <select id="status" name="status">
+          <option value="">All</option>
+          <option value="CURRENT">Watching</option>
+          <option value="COMPLETE">Complete</option>
+          <option value="PLANNING">Planning</option>
+          <option value="DROPPED">Dropped</option>
+          <option value="PAUSED">Paused</option>
+        </select>
+        <br />
+        <br />
+
+        <input type="submit" value="Filter" />
+      </form>
+    </>
+  );
+};
