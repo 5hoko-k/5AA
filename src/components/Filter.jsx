61a0b21186eee68445eecfb3413ae616473d401d
Shoko Kimera <shokokimera@gmail.com>
2023-05-01 23:13:05 +0300

    add styles to filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,23 +1,24 @@
-
+import "../styles/Filter.css";
 
 const Filter = (props) => {
-
   return (
     <>
-      <form>
-        <label htmlFor="category">Status:</label>
-        <select id="status" name="status">
-          <option value="">All</option>
-          <option value="CURRENT">Watching</option>
-          <option value="COMPLETE">Complete</option>
-          <option value="PLANNING">Planning</option>
-          <option value="DROPPED">Dropped</option>
-          <option value="PAUSED">Paused</option>
-        </select>
-        <br />
-        <br />
+      <form className="filter-form">
+        <div>
+          <label htmlFor="category">Status:</label>
+          <select id="status" name="status">
+            <option value="">All</option>
+            <option value="CURRENT">Watching</option>
+            <option value="COMPLETE">Complete</option>
+            <option value="PLANNING">Planning</option>
+            <option value="DROPPED">Dropped</option>
+            <option value="PAUSED">Paused</option>
+          </select>
+        </div>
 
-        <input type="submit" value="Filter" />
+        <div>
+          <button type="submit">Filter</button>
+        </div>
       </form>
     </>
   );
