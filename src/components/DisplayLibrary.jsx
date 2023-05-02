bd9e9de4df6d6e8d5d96ba751673d24c00e9b56e
Shoko Kimera <shokokimera@gmail.com>
2023-05-02 19:42:42 +0300

    add styles and status badges for library list

--- src/components/DisplayLibrary.jsx
+++ src/components/DisplayLibrary.jsx
@@ -4,7 +4,6 @@ import Filter from "../components/Filter";
 import { TitleIsEnglish } from "../App";
 
 function DisplayLibrary(props) {
-
   return (
     <>
       <Filter />
@@ -24,20 +23,54 @@ function DisplayLibrary(props) {
 export default DisplayLibrary;
 
 const DisplayAnime = (props) => {
-  const isEnglish = useContext(TitleIsEnglish)
+  const isEnglish = useContext(TitleIsEnglish);
   return (
     <>
       {props.entries.map(({ mediaId, media, progress, score, status }) => (
         <>
           <div key={mediaId} className="anime">
-            <img alt="image" src={media.coverImage.large} />
+            <div class="image-container">
+              <img alt="image" src={media.coverImage.large} />
+              {status === "CURRENT" && (
+                <span
+                  className="badge top-right"
+                  style={{ backgroundColor: "blue" }}
+                ></span>
+              )}
+              {status === "COMPLETE" && (
+                <span
+                  className="badge top-right"
+                  style={{ backgroundColor: "green" }}
+                ></span>
+              )}
+              {status === "PLANNING" && (
+                <span
+                  className="badge top-right"
+                  style={{ backgroundColor: "pink" }}
+                ></span>
+              )}
+              {status === "DROPPED" && (
+                <span
+                  className="badge top-right"
+                  style={{ backgroundColor: "red" }}
+                ></span>
+              )}
+              {status === "PAUSED" && (
+                <span
+                  className="badge top-right"
+                  style={{ backgroundColor: "orange" }}
+                ></span>
+              )}
+
+
+              <span class="badge top-right"></span>
+            </div>
+
             <span>
               <em>ID: </em>
               <span>{media.id}</span>
             </span>
-            <span>
-              {isEnglish ? media.title.english : media.title.romaji}
-            </span>
+            <span>{isEnglish ? media.title.english : media.title.romaji}</span>
             <span>
               <em>Progess: </em>
               {progress}
