de6e4baf4efb81700bf87b2f923c04929ed173ad
Shoko Kimera <shokokimera@gmail.com>
2023-05-04 21:15:50 +0300

    feat: add filter library feature

--- src/components/DisplayLibrary.jsx
+++ src/components/DisplayLibrary.jsx
@@ -4,16 +4,29 @@ import Filter from "../components/Filter";
 import { TitleIsEnglish } from "../App";
 
 function DisplayLibrary(props) {
+  const [genre, setGenre] = useState();
+  const [format, setFormat] = useState();
+  const [status, setStatus] = useState();
+
+  const onFilter = (genre, status, format) => {
+    setGenre(genre);
+    console.log(genre)
+    setStatus(status);
+    console.log(status)
+    setFormat(format);
+    console.log(format)
+  };
+
   return (
     <>
-      <Filter />
+      <Filter onFilter={onFilter} />
 
       {props.loading && <p>Loading...</p>}
       {props.error && <p>Error : {error.message}</p>}
       {props.data &&
         props.data.MediaListCollection.lists.map(({ index, entries }) => (
-          <div key={index} className="gallery" >
-            <DisplayAnime entries={entries} />
+          <div key={index} className="gallery">
+            <DisplayAnime entries={entries} genre={genre} status={status} format={format}/>
           </div>
         ))}
     </>
@@ -24,9 +37,24 @@ export default DisplayLibrary;
 
 const DisplayAnime = (props) => {
   const isEnglish = useContext(TitleIsEnglish);
+
+  const filteredEntries = props.entries.filter((entry) => {
+    let matched = true;
+
+    if (props.genre && entry.media.genres.indexOf(props.genre) === -1) {
+      matched = false;
+    }
+    if (props.format && entry.media.format !== props.format) {
+      matched = false;
+    }
+    if (props.status && entry.status !== props.status) {
+      matched = false;
+    }
+    return matched;
+  });
   return (
     <>
-      {props.entries.map(({ mediaId, media, progress, score, status }) => (
+      {filteredEntries.map(({ mediaId, media, progress, score, status }) => (
         <>
           <div key={mediaId} className="">
             <div class="image-container">
@@ -40,7 +68,7 @@ const DisplayAnime = (props) => {
               {status === "COMPLETED" && (
                 <span
                   className="badge top-right"
-                  style={{ backgroundColor: "#008000" }}
+                  style={{ backgroundColor: "#2E8B57" }}
                 ></span>
               )}
               {status === "PLANNING" && (
@@ -62,7 +90,13 @@ const DisplayAnime = (props) => {
                 ></span>
               )}
             </div>
-            <span>{isEnglish ? (media.title.english ? media.title.english : media.title.romaji) : media.title.romaji}</span>
+            <span>
+              {isEnglish
+                ? media.title.english
+                  ? media.title.english
+                  : media.title.romaji
+                : media.title.romaji}
+            </span>
             <span>
               <em>Progess: </em>
               {progress}
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,7 @@
 import "../styles/Filter.css";
 import { useQuery, gql } from "@apollo/client";
+import { useState } from "react";
+
 const query = gql`
   query {
     User(id: 853967) {
@@ -17,20 +19,29 @@ const query = gql`
   }
 `;
 
-const Filter = () => {
+const Filter = (props) => {
   const { loading, error, data } = useQuery(query);
+  const [genre, setGenre] = useState();
+  const [format, setFormat] = useState();
+  const [status, setStatus] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onFilter(genre, status, format);
+  };
+
   return (
     <>
       {loading && <p>Loading...</p>}
       {error && <p>Error : {error.message}</p>}
       {data && (
-        <form className="filter-form">
+        <form className="filter-form" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="category">Status:</label>
-            <select id="status" name="status">
+            <select id="status" name="status" onChange={(e) => setStatus(e.target.value)}>
               <option value="">All</option>
               <option value="CURRENT">Watching</option>
-              <option value="COMPLETE">Complete</option>
+              <option value="COMPLETED">Complete</option>
               <option value="PLANNING">Planning</option>
               <option value="DROPPED">Dropped</option>
               <option value="PAUSED">Paused</option>
@@ -39,7 +50,7 @@ const Filter = () => {
 
           <div>
             <label htmlFor="genre">Genre:</label>
-            <select id="genre" name="genre">
+            <select id="genre" name="genre" onChange={(e) => setGenre(e.target.value)}>
               <option value="">All</option>
               {data.User.statistics.anime.genres.map(({ genre }) => (
                 <option value={genre}>{genre}</option>
@@ -49,7 +60,7 @@ const Filter = () => {
 
           <div>
             <label htmlFor="format">Type:</label>
-            <select id="format" name="format">
+            <select id="format" name="format" onChange={(e) => setFormat(e.target.value)}>
               <option value="">All</option>
               {data.User.statistics.anime.formats.map(({ format }) => (
                 <option value={format}>{format}</option>
