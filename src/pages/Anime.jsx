39f7ba10fbe63d47e29257877e191d09729bce7f
Shoko Kimera <shokokimera@gmail.com>
2023-05-06 23:32:53 +0300

    update request query

--- src/pages/Anime.jsx
+++ src/pages/Anime.jsx
@@ -2,7 +2,7 @@ import { useQuery, gql } from "@apollo/client";
 import { useParams } from "react-router-dom";
 
 const query = gql`
-  query ($id: Int){
+  query ($id: Int) {
     Media(id: $id) {
       title {
         english(stylised: true)
@@ -12,22 +12,60 @@ const query = gql`
       coverImage {
         large
       }
+      description(asHtml: true)
+      averageScore
+      episodes
+      streamingEpisodes {
+        title
+        thumbnail
+        url
+      }
+      genres
+      tags {
+        name
+        description
+      }
+      characters(role: MAIN) {
+        edges {
+          node {
+            name {
+              full
+              userPreferred
+            }
+            image {
+              medium
+            }
+          }
+          voiceActors(language: ENGLISH) {
+            name {
+              full
+            }
+            image {
+              medium
+            }
+          }
+        }
+      }
     }
   }
 `;
 
 export default function Anime(props) {
   const { animeId } = useParams();
-  const variables = { id: animeId }
+  const variables = { id: animeId };
 
-  const { loading, error, data } = useQuery(query, {variables: variables});
+  const { loading, error, data } = useQuery(query, { variables: variables });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
 
   return (
     <>
-    <img alt='banner Image' src={data.Media.bannerImage} style={{width: "100%"}}/>
+      <img
+        alt="banner Image"
+        src={data.Media.bannerImage}
+        style={{ width: "100%" }}
+      />
       <h1>{animeId}</h1>
       {console.log(data)}
     </>
