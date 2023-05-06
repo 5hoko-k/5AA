71eb3f0cb9f2e715473d3c833b8c7412aeec2a53
Shoko Kimera <shokokimera@gmail.com>
2023-05-06 18:10:53 +0300

    fix: destructuring assignment.of object returned by useParams

--- src/pages/Anime.jsx
+++ src/pages/Anime.jsx
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom"
+
 export default function Anime(props) {
+    const { animeId } = useParams()
     return (
         <>
-        
+        <h1>{animeId}</h1>
         </>
     )
 }
