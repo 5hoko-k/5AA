e84460d974c719003880395dce3c52e90d9d0ebc
Shoko Kimera <shokokimera@gmail.com>
2023-05-09 00:33:30 +0300

    add container in app component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import DisplayProgress from "../components/DisplayProgress";
 import DisplayStats from "../components/DisplayStats";
+import Container from "@mui/material/Container";
 
 function Home() {
   return (
     <>
-      <DisplayStats />
-      <DisplayProgress />
+      <Container maxWidth="md">
+        <DisplayStats />
+        <DisplayProgress />
+      </Container>
     </>
   );
 }
