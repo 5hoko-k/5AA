7e4aaf2b51e1a2514a56bb9f0b853f56ef34bcfc
Shoko Kimera <shokokimera@gmail.com>
2023-05-10 00:35:40 +0300

    add buttons to link in toolbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
-import "../styles/Nav.css"
+import "../styles/Nav.css";
 import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+
 function NavBar() {
-    return (
-        <>
-            <header>
+  return (
+    <>
+      {/* <header>
                 <nav>
                     <ul>
                     <li><Link to="/">Home</Link></li>
@@ -11,9 +13,22 @@ function NavBar() {
                     <li><Link to="/about">About</Link></li>
                     </ul>
                 </nav>
-            </header>
-        </>
-    )
+            </header> */}
+      <AppBar>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'center', gap: '30px', padding: '20px 50px' }} > 
+          <Button component={Link} to="/" color="inherit">
+            <Typography variant="h6">Home</Typography>
+          </Button>
+          <Button component={Link} to="/library" color="inherit">
+            <Typography variant="h6">Library</Typography>
+          </Button>
+          <Button component={Link} to="/about" color="inherit">
+            <Typography variant="h6">About</Typography>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 }
 
 export default NavBar;
