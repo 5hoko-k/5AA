8fdcfe81eab1a6fdd7eecdb34ebbd85c84b3d846
Shoko Kimera <shokokimera@gmail.com>
2023-05-15 19:57:37 +0300

    shade the banner image and place it at the top

--- src/pages/Anime.jsx
+++ src/pages/Anime.jsx
@@ -74,14 +74,24 @@ export default function Anime(props) {
   return (
     <>
       <Box
-        component="div"
         sx={{
-          // backgroundImage: `url(${bannerImage})`,
+          backgroundImage: `url(${bannerImage})`,
+          position: 'relative',
           backgroundSize: "cover",
           backgroundPosition: "center",
+          minHeight: "100vh",
         }}
       >
-        <img alt="banner Image" src={bannerImage} style={{ width: "100%" }} />
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)", // Adjust the transparency as desired (0.5 = 50% opacity)
+          }}
+        />
       </Box>
 
       <h1>{animeId}</h1>
