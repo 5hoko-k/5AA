33e45c677486c90bbc0e56503d08a7756b3a18e5
Shoko Kimera <shokokimera@gmail.com>
2023-05-17 16:52:09 +0300

    create new component

---
+++ src/components/AnimeNavBar.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export default function AnimeNavBar() {
+    return (
+        <>
+            <Typography variant="body1">
+                Kakashi is the greatest of them all!!
+            </Typography>
+        </>
+    )
+}
