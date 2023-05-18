257faebb464a340a58473ea866c08dc218c6a1d1
Shoko Kimera <shokokimera@gmail.com>
2023-05-18 18:05:06 +0300

    create tabs for navigation

--- src/components/AnimeNavBar.jsx
+++ src/components/AnimeNavBar.jsx
@@ -1,11 +1,30 @@
-import { Typography } from "@mui/material";
+import { Box, Tab } from "@mui/material";
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { useState } from "react";
 
 export default function AnimeNavBar() {
+    const [value, setValue] = useState('1');
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+  
     return (
-        <>
-            <Typography variant="body1">
-                Kakashi is the greatest of them all!!
-            </Typography>
-        </>
-    )
+      <Box sx={{ width: '100%', typography: 'body1' }}>
+        <TabContext value={value}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <TabList onChange={handleChange} aria-label="lab API tabs example">
+              <Tab label="Item One" value="1" />
+              <Tab label="Item Two" value="2" />
+              <Tab label="Item Three" value="3" />
+            </TabList>
+          </Box>
+          <TabPanel value="1">Item One</TabPanel>
+          <TabPanel value="2">Item Two</TabPanel>
+          <TabPanel value="3">Item Three</TabPanel>
+        </TabContext>
+      </Box>
+    );
 }
