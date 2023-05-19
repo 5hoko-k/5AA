c2f5a31b9d75fca1fdeebe523a0858ce454b459c
Shoko Kimera <shokokimera@gmail.com>
2023-05-19 18:16:31 +0300

    Update AnimeNavBar.jsx

--- src/components/AnimeNavBar.jsx
+++ src/components/AnimeNavBar.jsx
@@ -1,30 +1,32 @@
 import { Box, Tab } from "@mui/material";
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
 import { useState } from "react";
 
 export default function AnimeNavBar() {
-    const [value, setValue] = useState('1');
+  const [value, setValue] = useState("characters");
 
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
-  
-    return (
-      <Box sx={{ width: '100%', typography: 'body1' }}>
-        <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab label="Item One" value="1" />
-              <Tab label="Item Two" value="2" />
-              <Tab label="Item Three" value="3" />
-            </TabList>
-          </Box>
-          <TabPanel value="1">Item One</TabPanel>
-          <TabPanel value="2">Item Two</TabPanel>
-          <TabPanel value="3">Item Three</TabPanel>
-        </TabContext>
-      </Box>
-    );
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%", typography: "body1" }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Characters" value="characters" />
+            <Tab label="Episodes" value="episodes" />
+            <Tab label="Stats" value="stats" />
+            <Tab label="Related" value="related" />
+          </TabList>
+        </Box>
+        <TabPanel value="characters">Characters</TabPanel>
+        <TabPanel value="episodes">Episodes</TabPanel>
+        <TabPanel value="stats">Stats</TabPanel>
+        <TabPanel value="related">Related</TabPanel>
+      </TabContext>
+    </Box>
+  );
 }
