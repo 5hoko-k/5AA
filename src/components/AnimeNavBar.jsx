588b07877ece37e5ba1995d35749a4aa83a5b937
Shoko Kimera <shokokimera@gmail.com>
2023-05-19 18:28:15 +0300

    add components to tabPanels

--- src/components/AnimeNavBar.jsx
+++ src/components/AnimeNavBar.jsx
@@ -3,6 +3,10 @@ import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import { useState } from "react";
+import Characters from "./Characters";
+import Episodes from "./Episodes";
+import Stats from "./Stats";
+import Related from "./Related";
 
 export default function AnimeNavBar() {
   const [value, setValue] = useState("characters");
@@ -15,17 +19,17 @@ export default function AnimeNavBar() {
     <Box sx={{ width: "100%", typography: "body1" }}>
       <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <TabList onChange={handleChange} aria-label="lab API tabs example" variant="fullWidth">
             <Tab label="Characters" value="characters" />
             <Tab label="Episodes" value="episodes" />
             <Tab label="Stats" value="stats" />
             <Tab label="Related" value="related" />
           </TabList>
         </Box>
-        <TabPanel value="characters">Characters</TabPanel>
-        <TabPanel value="episodes">Episodes</TabPanel>
-        <TabPanel value="stats">Stats</TabPanel>
-        <TabPanel value="related">Related</TabPanel>
+        <TabPanel value="characters"><Characters /></TabPanel>
+        <TabPanel value="episodes"><Episodes /></TabPanel>
+        <TabPanel value="stats"><Stats /></TabPanel>
+        <TabPanel value="related"><Related /></TabPanel>
       </TabContext>
     </Box>
   );
