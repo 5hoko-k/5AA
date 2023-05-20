d49c1a76e5e4af7006215148ac5ebd2fbc650c16
Shoko Kimera <shokokimera@gmail.com>
2023-05-20 19:26:58 +0300

    use anime data context

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { AnimeData } from "../pages/Anime";
+
 export default function Stats() {
+    const { averageScore, totalEpisodes, genres, tags, episodes, characters } =
+    useContext(AnimeData);
     return (
         <>
         
