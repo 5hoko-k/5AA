672a42c62d198dca3c83d1b120ba631d59373f67
Shoko Kimera <shokokimera@gmail.com>
2023-05-20 19:26:39 +0300

    create character grid

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,7 +1,33 @@
+import { useContext } from "react";
+import { AnimeData } from "../pages/Anime";
+import { Box, Grid, Typography } from "@mui/material";
+
 export default function Characters() {
-    return (
-        <>
-        
-        </>
-    )
+  const { characters } = useContext(AnimeData);
+  // console.log(characters.edges)
+  return (
+    <>
+      <Grid container>
+        {characters.edges.map(({ node, voiceActors }) =>
+          voiceActors.map(({ image, name }) => (
+            <Grid item>
+              <img
+                src={node.image.medium}
+                alt={node.name.full}
+              />
+
+              <Typography variant="body2">{node.name.full}</Typography>
+
+              <Typography variant="body2">{name.full}</Typography>
+
+              <img
+                src={image.medium}
+                alt={name.full}
+              />
+            </Grid>
+          ))
+        )}
+      </Grid>
+    </>
+  );
 }
