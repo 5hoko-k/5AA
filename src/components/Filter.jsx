f15a89524678fd46f19671f89fe254d655b1d16e
Shoko Kimera <shokokimera@gmail.com>
2023-05-22 15:21:20 +0300

    change filter styles and add paper component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -5,6 +5,7 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import { Button, Paper } from "@mui/material";
 
 const query = gql`
   query {
@@ -39,67 +40,71 @@ const Filter = (props) => {
       {loading && <p>Loading...</p>}
       {error && <p>Error : {error.message}</p>}
       {data && (
-        <form className="filter-form" onSubmit={handleSubmit}>
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="status">Status</InputLabel>
-            <Select
-              labelId="status"
-              id="status"
-              value={format}
-              onChange={(e) => setStatus(e.target.value)}
-              label="status"
-            >
-              <MenuItem value="">
-                <em>All</em>
-              </MenuItem>
-              <MenuItem value="CURRENT">Watching</MenuItem>
-              <MenuItem value="COMPLETED">Complete</MenuItem>
-              <MenuItem value="PLANNING">Planning</MenuItem>
-              <MenuItem value="DROPPED">Dropped</MenuItem>
-              <MenuItem value="PAUSED">Paused</MenuItem>
-            </Select>
-          </FormControl>
+        <Paper elevation={3}>
+          <form className="filter-form" onSubmit={handleSubmit}>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="status">Status</InputLabel>
+              <Select
+                labelId="status"
+                id="status"
+                value={format}
+                onChange={(e) => setStatus(e.target.value)}
+                label="status"
+              >
+                <MenuItem value="">
+                  <em>All</em>
+                </MenuItem>
+                <MenuItem value="CURRENT">Watching</MenuItem>
+                <MenuItem value="COMPLETED">Complete</MenuItem>
+                <MenuItem value="PLANNING">Planning</MenuItem>
+                <MenuItem value="DROPPED">Dropped</MenuItem>
+                <MenuItem value="PAUSED">Paused</MenuItem>
+              </Select>
+            </FormControl>
 
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="genre">Genre</InputLabel>
-            <Select
-              labelId="genre"
-              id="genre"
-              value={genre}
-              onChange={(e) => setGenre(e.target.value)}
-              label="genre"
-            >
-              <MenuItem value="">
-                <em>All</em>
-              </MenuItem>
-              {data.User.statistics.anime.genres.map(({ genre }) => (
-                <MenuItem value={genre}>{genre}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="genre">Genre</InputLabel>
+              <Select
+                labelId="genre"
+                id="genre"
+                value={genre}
+                onChange={(e) => setGenre(e.target.value)}
+                label="genre"
+              >
+                <MenuItem value="">
+                  <em>All</em>
+                </MenuItem>
+                {data.User.statistics.anime.genres.map(({ genre }) => (
+                  <MenuItem value={genre}>{genre}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
 
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-            <InputLabel id="type">Type</InputLabel>
-            <Select
-              labelId="type"
-              id="format"
-              value={format}
-              onChange={(e) => setFormat(e.target.value)}
-              label="type"
-            >
-              <MenuItem value="">
-                <em>All</em>
-              </MenuItem>
-              {data.User.statistics.anime.formats.map(({ format }) => (
-                <MenuItem value={format}>{format}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id="type">Type</InputLabel>
+              <Select
+                labelId="type"
+                id="format"
+                value={format}
+                onChange={(e) => setFormat(e.target.value)}
+                label="type"
+              >
+                <MenuItem value="">
+                  <em>All</em>
+                </MenuItem>
+                {data.User.statistics.anime.formats.map(({ format }) => (
+                  <MenuItem value={format}>{format}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
 
-          <div>
-            <button type="submit">Filter</button>
-          </div>
-        </form>
+            <div>
+              <Button variant="contained" type="submit">
+                Filter
+              </Button>
+            </div>
+          </form>
+        </Paper>
       )}
     </>
   );
