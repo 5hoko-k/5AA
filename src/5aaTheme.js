cdc6f746f445f07a1b0f2718de75db29c4b1c1ff
Shoko Kimera <shokokimera@gmail.com>
2023-11-20 15:05:51 +0300

    create theme file

---
+++ src/5aaTheme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme(
+    {
+        palette: {
+          primary: {
+            main: '#126936'
+          },
+          secondary: {
+            main: '#744fa4',
+          }
+        }
+      }
+  )
