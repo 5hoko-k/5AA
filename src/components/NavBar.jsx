f45f9326fc0465333cd7f7d02e6fda0720b7ac4d
Shoko Kimera <shokokimera@gmail.com>
2024-03-13 21:20:16 +0300

    add new popover component for navigation menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,7 @@
 import "../styles/Nav.css";
 import { Link } from "react-router-dom";
 import { AppBar, Toolbar, Typography, Button } from "@mui/material";
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
-import { Popover } from "@headlessui/react";
+import MobilePopover from "./mobilePopover";
 
 function NavBar() {
   return (
@@ -27,25 +26,7 @@ function NavBar() {
           </Button>
         </Toolbar>
       </AppBar>
-      <div className=" relative flex h-20 justify-between px-3 bg-teal-200">
-        <div className="self-center">logo</div>
-        <Popover className="relative self-center">
-          <Popover.Button className="inline-flex items-center p-3 bg-white rounded-md border hover:bg-slate-100">
-            Kakashi
-            <ChevronDownIcon className="h-3 w-3" aria-hidden="true" />
-          </Popover.Button>
-          <Popover.Panel className="absolute z-10">
-            <div className="grid grid-cols-2">
-              <a href="/analytics">Analytics</a>
-              <a href="/engagement">Engagement</a>
-              <a href="/security">Security</a>
-              <a href="/integrations">Integrations</a>
-            </div>
-
-            <img src="/solutions.jpg" alt="" />
-          </Popover.Panel>
-        </Popover>
-      </div>
+      <MobilePopover />
     </>
   );
 }
---
+++ src/components/mobilePopover.jsx
@@ -0,0 +1,36 @@
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { Popover } from "@headlessui/react";
+import { Button } from "@mui/material";
+
+function MobilePopover() {
+  return (
+    <>
+      <div className=" relative flex h-20 justify-center px-3 bg-teal-200">
+        {/* <div className="self-center">logo</div> */}
+        <Popover className="relative self-center">
+          {({ open }) => (
+            <>
+              <Popover.Button className="inline-flex items-center p-3 bg-white rounded-md hover:bg-slate-100 hover:ring-1">
+                Kakashi
+                <ChevronDownIcon className={`h-3 w-3 ${open ? 'rotate-180 transform-gpu' : ''}`} aria-hidden="true" />
+              </Popover.Button>
+              <Popover.Panel className="absolute -inset-x-24 z-10 top-16 bg-white rounded-md p-5 ring-1">
+                <div className="grid grid-cols-2">
+                  <a href="/analytics">Analytics</a>
+                  <a href="/engagement">Engagement</a>
+                  <a href="/security">Security</a>
+                  <a href="/integrations">Integrations</a>
+                </div>
+
+                <img src="/solutions.jpg" alt="" />
+              </Popover.Panel>
+            </>
+          )}
+        </Popover>
+      </div>
+    </>
+  );
+}
+
+export default MobilePopover;
+
