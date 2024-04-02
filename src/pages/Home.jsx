ce81a448e736cdd75406163becc5e8dd6c1e431d
Shoko Kimera <shokokimera@gmail.com>
2024-04-02 13:56:37 +0300

    add hero image and update navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import TheCharts from "../components/Home Components/Charts";
 import DisplayProgress from "../components/Home Components/DisplayProgress";
 import DisplayStats from "../components/DisplayStats";
+import heroImage from "../assets/heroImage.jpg";
 
 function Home() {
   return (
-    <div className="bg-slate-100 p-5">
+    <div className="bg-slate-100">
       {/* <DisplayStats /> */}
+      <div
+        className='h-svh bg-cover bg-center'
+        style={{ backgroundImage: `url(${heroImage})`}}
+      ></div>
       <TheCharts />
       <DisplayProgress />
     </div>
